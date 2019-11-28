d8a00c5243003cac0d83d4d754e4eb583741cfcb
jcanuman94 <ignaciocanuman@gmail.com>
2019-11-28 17:49:42 -0300

    Evaluacion Final android studio del 29 de noviembre

--- app/src/main/java/com/example/finalevalucion_jose/MainActivity.java
+++ app/src/main/java/com/example/finalevalucion_jose/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main)
         ConexionSQLiteHelper con=new ConexionSQLiteHelper(this,"bd_usuarios",null,1);
     }
     public void onClick(View view){
